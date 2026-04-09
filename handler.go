04e10eb5ba86ca869544fbbb70c59efdc555dac9
Marcus Ong <ong1.marcusb@gmail.com>
2026-04-09 14:48:12 +0800

    ⚡ add set and get commands

--- handler.go
+++ handler.go
@@ -1,8 +1,14 @@
 package main
 
+import (
+	"sync"
+)
+
 // map of command name (string) to handler function (func([]Value) Value)
 var Handlers = map[string]func([]Value) Value{
 	"PING": ping,
+	"SET":  set,
+	"GET":  get,
 }
 
 func ping(args []Value) Value {
@@ -11,4 +17,40 @@ func ping(args []Value) Value {
 	}
 
 	return Value{typ: "string", str: args[0].bulk}
+}
+
+var SETs = map[string]string{}
+var SETsMu = sync.RWMutex{} // basic RW mutext to protect the map
+
+func set(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'set' command"}
+	}
+
+	key := args[0].bulk
+	value := args[1].bulk
+
+	SETsMu.Lock()
+	SETs[key] = value
+	SETsMu.Unlock()
+
+	return Value{typ: "string", str: "OK"}
+}
+
+func get(args []Value) Value {
+	if len(args) != 1 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'get' command"}
+	}
+
+	key := args[0].bulk
+
+	SETsMu.RLock()
+	value, ok := SETs[key]
+	SETsMu.RUnlock()
+
+	if !ok {
+		return Value{typ: "err", bulk: "ERR value not found"}
+	}
+
+	return Value{typ: "bulk", bulk: value}
 }
