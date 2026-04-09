4f0745c6952144111a239e5165ff3a56980858c0
Marcus Ong <ong1.marcusb@gmail.com>
2026-04-09 15:12:49 +0800

    ⚡ add hset, hget and hgetall commands

--- handler.go
+++ handler.go
@@ -6,9 +6,12 @@ import (
 
 // map of command name (string) to handler function (func([]Value) Value)
 var Handlers = map[string]func([]Value) Value{
-	"PING": ping,
-	"SET":  set,
-	"GET":  get,
+	"PING":    ping,
+	"SET":     set,
+	"GET":     get,
+	"HSET":    hset,
+	"HGET":    hget,
+	"HGETALL": hgetall,
 }
 
 func ping(args []Value) Value {
@@ -53,4 +56,71 @@ func get(args []Value) Value {
 	}
 
 	return Value{typ: "bulk", bulk: value}
+}
+
+// Hash Sets - Hash Map within Hash Map
+
+var HSETs = map[string]map[string]string{}
+var HSETsMu = sync.RWMutex{}
+
+func hset(args []Value) Value {
+	if len(args) != 3 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'hset' command"}
+	}
+
+	hash := args[0].bulk
+	key := args[1].bulk
+	value := args[2].bulk
+
+	HSETsMu.Lock()
+	if _, ok := HSETs[hash]; !ok {
+		HSETs[hash] = map[string]string{}
+	}
+	HSETs[hash][key] = value
+	HSETsMu.Unlock()
+
+	return Value{typ: "string", str: "OK"}
+}
+
+func hget(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'hget' command"}
+	}
+
+	hash := args[0].bulk
+	key := args[1].bulk
+
+	HSETsMu.RLock()
+	value, ok := HSETs[hash][key]
+	HSETsMu.RUnlock()
+
+	if !ok {
+		return Value{typ: "null"}
+	}
+
+	return Value{typ: "bulk", bulk: value}
+}
+
+// Redis command that gets all the key-value pairs in a hash set
+func hgetall(args []Value) Value {
+	if len(args) != 1 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'hgetall' command"}
+	}
+
+	hash := args[0].bulk
+
+	HSETsMu.RLock()
+	values := HSETs[hash]
+	HSETsMu.RUnlock()
+
+	if values == nil {
+		return Value{typ: "array", array: []Value{}}
+	}
+
+	var array []Value
+	for key, value := range values {
+		array = append(array, Value{typ: "bulk", bulk: key + ": " + value})
+	}
+
+	return Value{typ: "array", array: array}
 }
